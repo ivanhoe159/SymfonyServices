83ab0e501457774a5bab7750284f5985c7781650
Anton <87985384+ivanhoe159@users.noreply.github.com>
2022-10-21 21:34:57 +0700

    Add files via upload

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,7 @@ use App\Entity\Service;
 use App\Entity\Usermoney;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -30,6 +31,20 @@ class MainController extends AbstractController {
         $budget = $repository->findAll();
         return $this->render('service.html.twig', [
             'services' => $services,
+            'budget' => $budget,
+        ]);
+    }
+
+    public function addMoney(ManagerRegistry $doctrine, Request $request)
+    {
+        $id = 1;
+        $entity = $doctrine->getManager();
+        $balance = $doctrine->getRepository(usermoney::class)->find($id);
+        $balance->setBalance($balance->getBalance() + $request->request->get('summ'));
+        $entity->flush();
+        $repository = $doctrine->getRepository(usermoney::class);
+        $budget = $repository->findAll();
+        return $this->render('balance.html.twig', [
             'budget' => $budget,
         ]);
     }
