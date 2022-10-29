30057bdc5f6dbf482b3c0c11d7311becc1309a43
Anton <87985384+ivanhoe159@users.noreply.github.com>
2022-10-29 23:23:50 +0700

    Add files via upload

--- service.sql
+++ service.sql
@@ -1,226 +1,163 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Окт 26 2022 г., 08:28
--- Версия сервера: 5.6.51
--- Версия PHP: 7.2.34
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: service
+-- ------------------------------------------------------
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- База данных: `service`
+-- Table structure for table `doctrine_migration_versions`
 --
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `doctrine_migration_versions`
---
-
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `doctrine_migration_versions` (
-  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `version` varchar(191) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `executed_at` datetime DEFAULT NULL,
-  `execution_time` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `execution_time` int DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `doctrine_migration_versions`
+-- Dumping data for table `doctrine_migration_versions`
 --
 
-INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20221020051003', '2022-10-20 07:10:27', 81),
-('DoctrineMigrations\\Version20221020105510', '2022-10-20 12:55:22', 84),
-('DoctrineMigrations\\Version20221022074705', '2022-10-22 09:47:16', 187);
-
--- --------------------------------------------------------
+LOCK TABLES `doctrine_migration_versions` WRITE;
+/*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
+INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20221020051003','2022-10-20 07:10:27',81),('DoctrineMigrations\\Version20221020105510','2022-10-20 12:55:22',84),('DoctrineMigrations\\Version20221022074705','2022-10-22 09:47:16',187);
+/*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Структура таблицы `messenger_messages`
+-- Table structure for table `messenger_messages`
 --
 
+DROP TABLE IF EXISTS `messenger_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `messenger_messages` (
-  `id` bigint(20) NOT NULL,
-  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL,
   `available_at` datetime NOT NULL,
-  `delivered_at` datetime DEFAULT NULL
+  `delivered_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  KEY `IDX_75EA56E016BA31DB` (`delivered_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Структура таблицы `service`
+-- Dumping data for table `messenger_messages`
 --
 
+LOCK TABLES `messenger_messages` WRITE;
+/*!40000 ALTER TABLE `messenger_messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messenger_messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service`
+--
+
+DROP TABLE IF EXISTS `service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `service` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `cost` double NOT NULL,
-  `active` int(11) NOT NULL,
-  `value` int(11) NOT NULL,
-  `maximvalue` int(11) NOT NULL,
-  `unit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `active` int NOT NULL,
+  `value` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `service`
+-- Dumping data for table `service`
 --
 
-INSERT INTO `service` (`id`, `name`, `cost`, `active`, `value`, `maximvalue`, `unit`) VALUES
-(1, 'Вывоз мусора', 165, 1, 1, 1, '-'),
-(2, 'Холодная вода', 16, 1, 33, 1000, 'м3'),
-(3, 'Горячая вода', 24, 1, 200, 1000, 'м3'),
-(4, 'Электроснабжение', 9, 1, 1, 10000, 'кв'),
-(5, 'Газоснабжение', 12, 0, 1, 1000, 'м3'),
-(6, 'Отопление', 30, 0, 1, 10000, 'Гкл'),
-(7, 'Содержание общедомового имущества', 136, 0, 1, 1, '-'),
-(8, 'Содержание лифта', 155, 0, 1, 1, '-');
-
--- --------------------------------------------------------
+LOCK TABLES `service` WRITE;
+/*!40000 ALTER TABLE `service` DISABLE KEYS */;
+INSERT INTO `service` VALUES (1,'Вывоз мусора',165,1,1),(2,'Холодная вода',16,1,1),(3,'Горячая вода',24,1,1),(4,'Электроснабжение',9,1,1),(5,'Газоснабжение',12,0,1),(6,'Отопление',30,0,1),(7,'Содержание общедомового имущества',136,0,1),(8,'Содержание лифта',155,0,1);
+/*!40000 ALTER TABLE `service` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Структура таблицы `transactions`
+-- Table structure for table `transactions`
 --
 
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `transactions` (
-  `id` int(11) NOT NULL,
-  `service` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `transname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `service` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `transname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `transdate` date NOT NULL,
   `cost` double NOT NULL,
-  `balance` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `balance` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=258 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `transactions`
+-- Dumping data for table `transactions`
 --
 
-INSERT INTO `transactions` (`id`, `service`, `transname`, `transdate`, `cost`, `balance`) VALUES
-(205, 'Отопление', 'Подписка на услугу', '2022-10-26', -4.839, 94659.613),
-(206, 'Отопление', 'Отписка от услуги', '2022-10-26', 4.839, 94664.452),
-(207, 'Вывоз мусора', 'Подписка на услугу', '2022-10-26', -26.613, 94637.839),
-(208, 'Вывоз мусора', 'Отписка от услуги', '2022-10-26', 26.613, 94664.452),
-(209, 'Вывоз мусора', 'Подписка на услугу', '2022-10-26', -26.613, 94637.839),
-(210, 'Вывоз мусора', 'Отписка от услуги', '2022-10-26', 26.613, 94664.452),
-(211, 'Вывоз мусора', 'Подписка на услугу', '2022-10-26', -798.387, 93866.065),
-(212, 'Вывоз мусора', 'Отписка от услуги', '2022-10-26', 798.387, 94664.452),
-(213, 'Пополнение счета', 'Зачисление денег', '2022-10-26', 10000, 10001),
-(214, 'Вывоз мусора', 'Подписка на услугу', '2022-10-26', -26.613, 9974.387),
-(215, 'Холодная вода', 'Подписка на услугу', '2022-10-26', -80, 9894.387),
-(216, 'Пополнение счета', 'Зачисление денег', '2022-10-26', 896, 10790.387),
-(217, 'Холодная вода', 'Отписка от услуги', '2022-10-26', 80, 10870.387),
-(218, 'Вывоз мусора', 'Отписка от услуги', '2022-10-26', 26.613, 10897),
-(219, 'Вывоз мусора', 'Подписка на услугу', '2022-10-26', -26.613, 10870.387),
-(220, 'Вывоз мусора', 'Плата за услуги', '2022-10-26', -165, 10705.387),
-(221, 'Холодная вода', 'Подписка на услугу', '2022-10-26', -85.161, 10620.226),
-(222, 'Горячая вода', 'Подписка на услугу', '2022-10-26', -774.194, 9846.032),
-(223, 'Вывоз мусора', 'Плата за услуги', '2022-10-26', -165, 9681.032),
-(224, 'Холодная вода', 'Плата за услуги', '2022-10-26', -528, 9153.032),
-(225, 'Горячая вода', 'Плата за услуги', '2022-10-26', -4800, 4353.032),
-(226, 'Вывоз мусора', 'Плата за услуги', '2022-10-26', -165, 4188.032),
-(227, 'Холодная вода', 'Плата за услуги', '2022-10-26', -528, 3660.032),
-(228, 'Горячая вода', 'Плата за услуги', '2022-10-26', -4800, -1139.968),
-(229, 'Пополнение счета', 'Зачисление денег', '2022-10-26', 1000, -139.968),
-(230, 'Пополнение счета', 'Зачисление денег', '2022-10-26', 10000, 9860.032),
-(231, 'Электроснабжение', 'Подписка на услугу', '2022-10-26', -1.452, 0.548);
-
--- --------------------------------------------------------
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (233,'Электроснабжение','Отписка от услуги','2022-10-29',0.581,10001.129),(234,'Горячая вода','Отписка от услуги','2022-10-29',309.677,10310.806),(235,'Холодная вода','Отписка от услуги','2022-10-29',34.065,10344.871),(236,'Вывоз мусора','Отписка от услуги','2022-10-29',10.645,10355.516),(237,'Пополнение счета','Зачисление денег','2022-10-29',56,10411.516),(238,'Вывоз мусора','Подписка на услугу','2022-10-29',-1064.516,9347),(239,'Вывоз мусора','Отписка от услуги','2022-10-29',1064.516,10411.516),(240,'Отопление','Подписка на услугу','2022-10-29',-1.935,10409.581),(241,'Отопление','Отписка от услуги','2022-10-29',1.935,10411.516),(242,'Вывоз мусора','Подписка на услугу','2022-10-29',-10.645,10400.871),(243,'Вывоз мусора','Плата за услуги','2022-10-29',-165,10235.871),(244,'Холодная вода','Подписка на услугу','2022-10-29',-1.032,10234.839),(245,'Горячая вода','Подписка на услугу','2022-10-29',-1.548,10233.291),(246,'Электроснабжение','Подписка на услугу','2022-10-29',-0.581,10232.71),(247,'Газоснабжение','Подписка на услугу','2022-10-29',-9.29,10223.42),(248,'Газоснабжение','Отписка от услуги','2022-10-29',9.29,10232.71),(249,'Пополнение счета','Зачисление денег','2022-10-29',1500,11732.71),(250,'Вывоз мусора','Плата за услуги','2022-10-29',-165,11567.71),(251,'Холодная вода','Плата за услуги','2022-10-29',-16,11551.71),(252,'Горячая вода','Плата за услуги','2022-10-29',-24,11527.71),(253,'Электроснабжение','Плата за услуги','2022-10-29',-9,11518.71),(254,'Вывоз мусора','Плата за услуги','2022-10-29',-165,11353.71),(255,'Холодная вода','Плата за услуги','2022-10-29',-16,11337.71),(256,'Горячая вода','Плата за услуги','2022-10-29',-24,11313.71),(257,'Электроснабжение','Плата за услуги','2022-10-29',-9,11304.71);
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Структура таблицы `usermoney`
+-- Table structure for table `usermoney`
 --
 
+DROP TABLE IF EXISTS `usermoney`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usermoney` (
-  `id` int(11) NOT NULL,
-  `login` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `balance` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `balance` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `usermoney`
+-- Dumping data for table `usermoney`
 --
 
-INSERT INTO `usermoney` (`id`, `login`, `balance`) VALUES
-(1, 'user', 0.548);
+LOCK TABLES `usermoney` WRITE;
+/*!40000 ALTER TABLE `usermoney` DISABLE KEYS */;
+INSERT INTO `usermoney` VALUES (1,'user',11304.71);
+/*!40000 ALTER TABLE `usermoney` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `doctrine_migration_versions`
---
-ALTER TABLE `doctrine_migration_versions`
-  ADD PRIMARY KEY (`version`);
-
---
--- Индексы таблицы `messenger_messages`
---
-ALTER TABLE `messenger_messages`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
-  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
-  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
-
---
--- Индексы таблицы `service`
---
-ALTER TABLE `service`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `transactions`
---
-ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `usermoney`
---
-ALTER TABLE `usermoney`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `messenger_messages`
---
-ALTER TABLE `messenger_messages`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `service`
---
-ALTER TABLE `service`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT для таблицы `transactions`
---
-ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=232;
-
---
--- AUTO_INCREMENT для таблицы `usermoney`
---
-ALTER TABLE `usermoney`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-29 23:22:21
